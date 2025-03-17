5a3e687466e9e4aee7d1cf692b272d60687bdbc6
QuyNg13 <quythieu34@gmail.com>
2025-03-17 16:02:41 +0100

    slider fixings

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-// ONDERSTAANDE CODE MAG JE GEBRUIKEN
 
-const ranges = document.querySelectorAll('[type="range"]');
 
-const updateRange = range => {
-	// the name of the custom property is the name of the input
-	const rangeName = range.name;
-	// the value of the custom property is the value of the input
-	const rangeValue = range.value;
-	// a custom property is set on the HTML element
-	document.documentElement.style.setProperty("--"+rangeName, rangeValue);
-}
+// const ranges = document.querySelectorAll('[type="range"]');
 
-ranges.forEach(range => {
-	// the custom property is set initially after loading the document
-	updateRange(range);
+// const updateRange = range => {
+
+// 	const rangeName = range.name;
+
+// 	const rangeValue = range.value;
+
+// 	document.documentElement.style.setProperty("--"+rangeName, rangeValue);
+// }
+
+// ranges.forEach(range => {
+// 	updateRange(range);
 	
-	// the custom property is updated when the input is changed
-	range.oninput = () => {
-		updateRange(range)
-	};
-});
+// 	range.oninput = () => {
+// 		updateRange(range)
+// 	};
+// });
